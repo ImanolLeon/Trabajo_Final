0434fe7e50fc82cf88598b40989bdae54ee8b5ef
MartinRams <alexanderamsyam@gmail.com>
2023-10-24 15:45:05 -0500

    Se ha amejorado la clase Conexion.

--- ui/src/main/java/com/enciclopedia/animales/conexion/Conexion.java
+++ ui/src/main/java/com/enciclopedia/animales/conexion/Conexion.java
@@ -10,19 +10,33 @@ import java.util.List;
 public class Conexion {
     List<Animal> datos= new ArrayList<>();
 
-    File file =new File("Animales.csv");
     public void leerCsv (){
+        // Establecemos la ruta absoluta del CSV
+        String ruta = System.getProperty("user.dir") +
+                "/ui/src/main/java/com/enciclopedia/animales/conexion/Animales.csv";
+
+        File file =new File(ruta);
+
         try{
+            // Cargamos en memoria el archivo
             FileReader FileRider= new FileReader(file);
             BufferedReader bufferedReader=new BufferedReader(FileRider);
+
+            // Evitamos leer los cabezales
             bufferedReader.readLine();
+
             String fila;
 
-            while((fila=bufferedReader.readLine()) != null){
-                String valores[]= fila.split(",");
+            while((fila = bufferedReader.readLine()) != null){
+                String[] valores = fila.split(",");  // Separamos los valores por comas
+
+                // Añadimos los valores a la lista
                 datos.add(new Animal(valores[0],valores[1],valores[2],valores[3],valores[4]));
             }
-        } catch (IOException e ){}
+
+        } catch (IOException e ){
+            System.out.println("Ha ocurrido un error al cargar el CSV");
+        }
     }
 
     public List<Animal> getDatos() {
